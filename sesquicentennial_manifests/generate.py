46e259a085760c33b2eed3d9d6fc7768d8d16231
Mark Baggett <markpbaggett@gmail.com>
2025-12-03 19:08:35 -0600

    Add changes.

--- sesquicentennial_manifests/generate.py
+++ sesquicentennial_manifests/generate.py
@@ -2,19 +2,21 @@ from csv import DictReader
 from manifest import TamuManifest
 
 all_data = []
-with open('sesquicentennial_images.csv', 'r') as data_csv:
+with open('sesq-merged-and-based.csv', 'r') as data_csv:
     csv_reader = DictReader(data_csv)
     for row in csv_reader:
         if row['Building Name'] != "":
-            all_data.append(
-                {
-                    "label": row['Building Name'],
-                    "filename": row['Filename'],
-                    "coords": row['Coordinates'],
-                    "date": row['Date'],
-                    'caption': row['Notes/Description/Caption']
-                }
-            )
+            if row["Photograph Date"] != "":
+                all_data.append(
+                    {
+                        "label": row['Building Name'],
+                        "filename": row['Filename'],
+                        "coords": row['Coordinates'],
+                        "date": row['Photograph Date'],
+                        'caption': row['Our Description / Notes'],
+                        'based': row['based']
+                    }
+                )
 for row in all_data:
     x = TamuManifest(row, ["navPlace.json"])
     x.write()
